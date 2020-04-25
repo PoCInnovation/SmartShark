90d7cfff378df4a7503871e7ae4b89bb375c712d
Valentin De Matos <valentin.de-matos@epitech.eu>
2020-04-25 21:20:29 +0200

    Add script to select only benign or not benign files to improve dataset

---
+++ datasets/DDoS/UNB/script/ds_get_only_benign_or_not_benign_packets.py
@@ -0,0 +1,40 @@
+###############################################
+
+#                                             #
+#              PoC: Smartshark                #
+#                                             #
+#                  USAGE:                     #
+#                                             #
+#        Script to clean datasets             #
+#                                             #
+#  Print only the asked rows (benign or not)  #
+#                                             #
+###############################################
+
+import sys
+
+#Choose your file
+fichier = open("./the_file.csv", "r")
+
+wanted = 0
+while (wanted != '1\n' and wanted != '2\n'):
+    print("Press 1 to get only benigns packets and 2 to get DS whithout banigns packets\n\tYour choice: ", end="")
+    wanted = sys.stdin.readline()
+
+
+def is_a_benign_packet(line):
+    if (len(line) >= len('benign\n') and line[len(line) - 1] == '\n' and line[len(line) - 2] == 'N' and line[len(line) - 3] == 'G' and line[len(line) - 4] == 'I' and line[len(line) - 5] == 'N' and line[len(line) - 6] == 'E' and line[len(line) - 7] == 'B'):
+        return True
+    elif (len(line) >= len('benign') and line[len(line) - 1] == 'N' and line[len(line) - 2] == 'G' and line[len(line) - 3] == 'I' and line[len(line) - 4] == 'N' and line[len(line) - 5] == 'E' and line[len(line) - 6] == 'B'):
+        return True
+    else:
+        return False
+
+result = ""
+for line in fichier:
+    if (wanted == '1\n' and is_a_benign_packet(line)):
+        result = result + line
+    elif (wanted == '2\n' and is_a_benign_packet(line) == False):
+        result = result + line
+print (result)
+fichier.close()
--- datasets/DDoS/UNB/script/ds_print_cleaned.py
+++ datasets/DDoS/UNB/script/ds_print_cleaned.py
@@ -16,7 +16,8 @@
 #                                             #
 ###############################################
 
-fichier = open("./Portmap.csv", "r")
+#Choose your file
+fichier = open("the_file.csv", "r")
 row = dict([('Unnamed', False), ('FlowID', False), ('SourceIP', True), ('SourcePort', True), ('DestinationIP', True), ('DestinationPort', True), ('Protocol', True), ('Timestamp', True), ('FlowDuration', False), ('TotalFwdPackets', False), ('TotalBackwardPackets', False), ('TotalLengthofFwdPackets', False), ('TotalLengthofBwdPackets', False), ('FwdPacketLengthMax', False), ('FwdPacketLengthMin', False), ('FwdPacketLengthMean', False), ('FwdPacketLengthStd', False), ('BwdPacketLengthMax', False), ('BwdPacketLengthMin', False), ('BwdPacketLengthMean', False), ('BwdPacketLengthStd', False), ('FlowBytes/s', False), ('FlowPackets/s', False), ('FlowIATMean', False), ('FlowIATStd', False), ('FlowIATMax', False), ('FlowIATMin', False), ('FwdIATTotal', False), ('FwdIATMean', False), ('FwdIATStd', False), ('FwdIATMax', False), ('FwdIATMin', False), ('BwdIATTotal', False), ('BwdIATMean', False), ('BwdIATStd', False), ('BwdIATMax', False), ('BwdIATMin', False), ('FwdPSHFlags', False), ('BwdPSHFlags', False), ('FwdURGFlags', False), ('BwdURGFlags', False), ('FwdHeaderLength', False), ('BwdHeaderLength', False), ('FwdPackets/s', False), ('BwdPackets/s', False), ('MinPacketLength', False), ('MaxPacketLength', False), ('PacketLengthMean', False), ('PacketLengthStd', False), ('PacketLengthVariance', False), ('FINFlagCount', False), ('SYNFlagCount', False), ('RSTFlagCount', False), ('PSHFlagCount', False), ('ACKFlagCount', False), ('URGFlagCount', False), ('CWEFlagCount', False), ('ECEFlagCount', False), ('Down/UpRatio', False), ('AveragePacketSize', False), ('AvgFwdSegmentSize', False), ('AvgBwdSegmentSize', False), ('FwdHeaderLength.1', False), ('FwdAvgBytes/Bulk', False), ('FwdAvgPackets/Bulk', False), ('FwdAvgBulkRate', False), ('BwdAvgBytes/Bulk', False), ('BwdAvgPackets/Bulk', False), ('BwdAvgBulkRate', False), ('SubflowFwdPackets', False), ('SubflowFwdBytes', False), ('SubflowBwdPackets', False), ('SubflowBwdBytes', False), ('Init_Win_bytes_forward', False), ('Init_Win_bytes_backward', False), ('act_data_pkt_fwd', False), ('min_seg_size_forward', False), ('ActiveMean', False), ('ActiveStd', False), ('ActiveMax', False), ('ActiveMin', False), ('IdleMean', False), ('IdleStd', False), ('IdleMax', False), ('IdleMin', False), ('SimillarHTTP', False), ('Inbound', False), ('Label', True)])
 
 def get_next_value(i, str, first):
@@ -42,7 +43,7 @@ for line in fichier:
         index += 1
     if (len(result) > 0 and result[len(result) - 1] != '\n'):
         result += '\n'
-    
+
 
 print(result, end="", sep="")
 fichier.close()
